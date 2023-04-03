205ace119f38f52b515f8900552f6e40e7442c40
vicentematus <vicentematusortiz@gmail.com>
2023-04-03 16:56:56 -0400

    chore: add types folder alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-		postcss: true
+		postcss: true,
 	}),
 
 	kit: {
 		adapter: adapter(),
 		alias: {
-			$components: './src/components/'
-		}
-	}
+			$components: "./src/components/",
+			$types: "./src/types/",
+		},
+	},
 };
 
 export default config;
