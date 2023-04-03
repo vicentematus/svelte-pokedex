a3bc6063e250ea075d2560cb05ed0b4424ec02b4
vicentematus <vicentematusortiz@gmail.com>
2023-04-03 03:04:21 -0400

    feat: i hope this doesnt break anything NES.CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
-	import Nav from '../components/nav.svelte';
 	import '../app.css';
 	import '@fontsource/press-start-2p';
+	import 'nes.css/css/nes.min.css';
 </script>
 
 <div class="bg-indigo-700 min-h-screen">
-	<Nav />
 	<div class="max-w-5xl mx-auto px-2 lg:px-4 py-2 lg:py-4 ">
 		<slot />
 	</div>
