83f5304484a1533bd1350dad5c0988f0c1259e03
vicentematus <vicentematusortiz@gmail.com>
2023-04-03 17:01:13 -0400

    feat: implement the store and improve performance. add footer too

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import { pokemon } from '../stores/pokestore';
-	import '../app.css';
 	import PokemonCard from '../components/PokemonCard.svelte';
+	import type { PokemonApi } from '../types/Pokemon';
+
 	let searchTerm = '';
-	let filteredPokemons: any = [];
+	let filteredPokemons: PokemonApi[] = [];
 
 	$: {
-		// Reacciona si cambia la variable
-		console.log(searchTerm);
-
 		if (searchTerm) {
 			filteredPokemons = $pokemon.filter((pokemon) =>
 				pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -21,7 +19,7 @@
 
 <img src="/img/pokeball.png" class="w-32 h-32 mx-auto" alt="" />
 <h1 class="text-3xl  text-white font-semibold uppercase text-center">SVELTE KIT POKEDEX</h1>
-<p class="text-white text-center">Search for your favorite pokemons. Using the pokeapi.</p>
+<p class="text-white text-center mt-2">Search for your favorite pokemons. Using the pokeapi.</p>
 <input
 	bind:value={searchTerm}
 	placeholder="Search for example: Ivysaur, Squirtle"
@@ -29,10 +27,7 @@
 	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
 />
 <div class="py-4 grid gap-4 md:grid-cols-2 lg:grid-cols-4 grid-cols-1">
-	{#each filteredPokemons as pokemon (pokemon.id)}
+	{#each filteredPokemons.slice(0, 20) as pokemon (pokemon.id)}
 		<PokemonCard {pokemon} />
 	{/each}
 </div>
-
-<style>
-</style>
--- src/stores/pokestore.ts
+++ src/stores/pokestore.ts
@@ -1,25 +1,4 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
+import type { PokemonApi } from "$types/Pokemon";
 
-export const pokemon = writable([]);
-function capitalizeFirstLetter(str: string) {
-	// converting first letter to uppercase
-	const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
-
-	return capitalized;
-}
-const fetchPokemon = async () => {
-	const url = `https://pokeapi.co/api/v2/pokemon?limit=1000`;
-	const res = await fetch(url);
-	const data = await res.json();
-	const loadedPokemon = data.results.map((data: any, index: any) => ({
-		name: capitalizeFirstLetter(data.name),
-		id: index + 1,
-		image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-			index + 1
-		}.png`
-	}));
-	console.log(loadedPokemon);
-	pokemon.set(loadedPokemon);
-};
-
-fetchPokemon();
+export const pokemon = writable<Partial<PokemonApi[]>>();
